6c744016c4d424654436b6f45cb322c4eba64221
S1NS3RO <douglassgbr@gmail.com>
2023-10-30 00:26:38 -0300

    part2

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,46 @@
+// Next
+import Image from 'next/image'
+
+// Componentes
+import LinkButton from '@/components/Utils/LinkButton'
+
+// icons
+import { BsArrowRight } from 'react-icons/bs'
+
+//styles
+import '@/styles/home.scss'
+
+export default function SuperGas() {
+  return (
+    <>
+      <div>
+        <div className='wrapper_img'>
+          <Image
+            src={'/image1.png'}
+            alt={'Imagem de um entregador de gás'}
+            width={480}
+            height={518}
+          />
+        </div>
+        <p className='wrapper_initial_text'>
+          <strong>Bem-vindo(a)</strong>, você está na central de vendas{' '}
+          <span className='decored_span__primary'>Gás 2 Irmãos</span>,
+          trabalhamos com um atendimento diferenciado para te oferecer o{' '}
+          <span className='decored_span__secondary'>melhor gás</span>,{' '}
+          <span className='decored_span__secondary'>máxima qualidade</span> e{' '}
+          <span className='decored_span__secondary'>preço justo</span>
+          .
+          <br />• Fazer o seu pedido é rápido e simples.
+          <br />• O gás chegará na sua casa em poucos minutos.
+        </p>
+      </div>
+      <div className='wrapper_button'>
+        <LinkButton
+          to={'/produtos'}
+          Icon={<BsArrowRight />}
+          text={'Avançar'}
+        />
+      </div>
+    </>
+  )
+}
---
+++ src/components/Utils/CustomDialog.tsx
@@ -0,0 +1,92 @@
+import '@/styles/CustomDialog.scss'
+
+// functions
+import { formatValues } from '@/functions/functions'
+
+//hooks
+import { useAppState } from '@/contexts/dadosCompra'
+
+//icons
+import { BsArrowLeft, BsWhatsapp} from "react-icons/bs"
+
+const CustomDialog = ({
+  showDialog,
+  onYesClick,
+  onNoClick,
+  productsSummary
+}) => {
+  const { clientData, valorTotal, selectedProducts } = useAppState()
+  return (
+    <div
+      className='custom_dialog'
+      style={{ display: showDialog ? 'flex' : 'none' }}>
+      <div className='internal'>
+        <h1 className='center'>Verifique as informações</h1>
+        <div>
+          {' '}
+          <p>
+            <strong>Cliente:</strong> {clientData.name}
+          </p>
+          <p>
+            <strong>Contato:</strong> {clientData.phone}
+          </p>
+          <p>
+            <strong>Endereço:</strong> {clientData.street},{' '}
+            {clientData.housenumber}
+          </p>
+          <p>
+            <strong>Compl.:</strong> {clientData.complement || 'Não'}
+          </p>
+          <p>
+            <strong>Bairro:</strong> {clientData.district}
+            {' - '}
+            {clientData.city}
+          </p>
+          <br />
+          <p>
+            <strong>Informações Adicionais:</strong>
+          </p>
+          <p>{clientData.additional || 'Não'}</p>
+          <br />
+          <p>
+            <strong>Produtos Selecionados:</strong>
+          </p>
+          <ul>
+            {selectedProducts.map(selectedProduct => (
+              <li key={selectedProduct.produto.id}>
+                {selectedProduct.selectedQuantity.toString().padStart(2, '0')}{' '}
+                {selectedProduct.produto.name}
+                {' - '}
+                {formatValues(
+                  selectedProduct.produto.price *
+                    selectedProduct.selectedQuantity
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <p className='center'>
+          <strong>
+            Total: {formatValues(valorTotal)}
+            {' - '}
+            {clientData.pay}
+          </strong>
+        </p>
+        <div className='buttons'>
+          <button
+            className='button'
+            onClick={onNoClick}>
+            <BsArrowLeft/> Revisar
+          </button>
+          <button
+            className='button'
+            onClick={onYesClick}>
+            <BsWhatsapp/> Avançar
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CustomDialog
